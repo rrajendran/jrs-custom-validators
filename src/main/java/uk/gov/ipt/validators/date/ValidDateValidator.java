93e32a14219c6c9737761f6171af6e848a367572
Ramesh Rajendran <rameshchinnu61@gmail.com>
2015-10-17 12:32:43 +0100

    Updates

---
+++ src/main/java/uk/gov/ipt/validators/date/ValidDateValidator.java
@@ -0,0 +1,33 @@
+package uk.gov.ipt.validators.date;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+
+/**
+ *
+ */
+public class ValidDateValidator implements ConstraintValidator<ValidDate, Object> {
+    ValidDate validDate = null;
+    public void initialize(ValidDate dateRange) {
+        validDate = dateRange;
+    }
+
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        if(o != null && o instanceof  String){
+            try {
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(validDate.format());
+                dateTimeFormatter.withResolverStyle(ResolverStyle.STRICT);
+                LocalDate.parse((String) o, dateTimeFormatter);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+               return false;
+            }
+        }
+
+        return false;
+    }
+}
--- src/test/java/com/capella/jsr/validators/date/ValidDateValidatorTest.java
+++ src/test/java/com/capella/jsr/validators/date/ValidDateValidatorTest.java
@@ -7,10 +7,12 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import java.util.Collection;
 import java.util.Set;
 
 import static java.util.Arrays.asList;
+import static javax.validation.Validation.buildDefaultValidatorFactory;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -20,12 +22,13 @@ import static org.junit.Assert.assertEquals;
 public class ValidDateValidatorTest {
     private String date;
     private boolean result;
+    private Validator validator = buildDefaultValidatorFactory().getValidator();
 
-    @Parameters(name = "{index}: Date - {0} isValid {1}")
+    @Parameters(name = "{index}: Date : {0}] Expected :{1}")
     public static Collection<Object[]> data() {
         return asList(new Object[][]{
                 {"01-01-2001", true},
-                {"31-09-2001", false}, // Max date 30 test
+                {"31-09-2015", false}, // Max date 30 test
                 {"29-02-2016", true}, // leap year test
                 {"29-02-2s16", false}, // characters in date
                 {"29-13-2016", false}, // invalid month
@@ -37,11 +40,11 @@ public class ValidDateValidatorTest {
         this.result = result;
     }
 
+
     @Test
     public void test(){
         DateObject date = DateObject.DateObjectBuilder.aDateObject().date(this.date).build();
         Set<ConstraintViolation<DateObject>> validate = CustomValidatorFactory.getValidator().validate(date);
-
         assertEquals(this.result, validate.size() == 0);
     }
 
